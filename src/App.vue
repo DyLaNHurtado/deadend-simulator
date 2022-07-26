efd2907d266bc79232f73ad702b46aa709c676d6
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-07-26 04:09:00 +0200

    Creating sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class=" bg-zinc-900 h-screen" >
   <header class="py-5 text-3xl flex items-center justify-between px-5 text-zinc-200 absolute w-full transition-all duration-500 bg-gray-900">
+    <fa icon="bars" class=""/>
     <span>DeadEnd Simulator</span>
     <div class="flex flex-row items-center justify-between ">
       <button class="px-5 py-5 bg-zinc-500 mx-2 rounded hover:bg-violet-500 hover:text-black transition-all duration-500  border-2 border-transparent hover:border-white">Sign up</button>
@@ -8,6 +9,52 @@
     </div>
   </header>
   <div class=" w-full h-full bg-pattern">
+    <div class="fixed w-full h-full bg-opacity-80 bg-black z-50">
+      <div class=" h-full w-full md:w-1/3 bg-gray-900 p-5 shadow-lg shadow-gray-900">
+        <div class="h-1/6 w-full">
+        <fa icon="xmark" class=" text-3xl text-white"/>
+
+        </div>
+        <div class="h-5/6 w-full">
+          <div class="h-3/4 w-full">
+            <div class="w-full flex flex-row md:flex-col lg:flex-col xl:flex-col 2xl:flex-row">
+              <div class=" border-2  border-violet-500 shadow-md shadow-violet-500 hover:shadow-yellow-500 flex flex-col hover:border-yellow-500 items-center justify-center group transition-all duration-500">
+          <fa icon="house-chimney" class=" text-3xl  text-violet-500  group-hover:text-yellow-500 transition-colors duration-700"/>
+          <div class="flex items-center justify-center text-violet-500 group-hover:text-yellow-500 transition-colors duration-700">
+            <span>Home</span>
+          </div>
+          </div>
+          <div class=" border-2 p-24 w-1/2 border-violet-500 flex flex-col hover:border-yellow-500 items-center justify-center group transition-colors duration-500">
+          <fa icon="book" class=" text-3xl  text-violet-500  group-hover:text-yellow-500 transition-colors duration-700"/>
+          <div class="flex items-center justify-center text-violet-500 group-hover:text-yellow-500 transition-colors duration-700">
+            <span>Instrucciones</span>
+          </div>
+          </div>
+            </div>
+          
+          <div class=" border-2  border-violet-500 flex flex-col hover:border-yellow-500 items-center justify-center group transition-colors duration-500">
+          <fa icon="play" class=" text-3xl  text-violet-500 group-hover:text-yellow-500 transition-colors duration-700"/>
+          <div class="flex items-center justify-center text-violet-500 group-hover:text-yellow-500 transition-colors duration-700">
+            <span>Play</span>
+          </div>
+          </div>
+          <div class=" border-2 border-violet-500 flex flex-col hover:border-yellow-500 items-center justify-center group transition-colors duration-500">
+          <fa icon="id-card" class=" text-3xl  text-violet-500 group-hover:text-yellow-500 transition-colors duration-700"/>
+          <div class="flex items-center justify-center text-violet-500 group-hover:text-yellow-500 transition-colors duration-700">
+            <span>Perfil</span>
+          </div>
+          </div>
+          </div>
+          <div class="h-1/4 w-full">
+            <fa icon="play" class=" text-3xl "/>
+
+          <fa icon="id-card" class=" text-3xl "/>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
     <ProfileView/>
   </div>
   <a href="https://github.com/DyLaNHurtado" target="_blank">
--- src/components/ProfileView.vue
+++ src/components/ProfileView.vue
@@ -2,8 +2,9 @@
 <div class="flex flex-row items-start pt-36 justify-center bg-slate-700 bg-opacity-80 h-full border-2 border-y-0 border-violet-500 mx-20 px-10">
   <div class="flex flex-col items-center justify-center h-full w-1/3">
     <div class="w-52 h-52 flex flex-col items-center justify-center">
-      <span class="text-yellow-500 font-bold text-5xl text-center w-full mb-8">User123</span>
       <img src="../assets/logo.png" class="rounded-lg border-4 border-violet-500 h-80 w-80 bg-gray-900 object-center" alt=""/> 
+      <span class="text-yellow-500 font-bold text-5xl text-center w-full mt-8">User123</span>
+
     </div>
     <div class="w-full h-1/2 flex flex-col text-center items-center justify-center">
       <span class="text-violet-500 font-bold text-4xl text-center w-1/2 my-3 border-4 border-violet-500 mb-10 p-4 bg-gray-900 rounded-3xl "> Stats:</span>
@@ -20,9 +21,9 @@
     </div>
   </div>
   <div class="w-2/3 h-full flex flex-col text-center items-center justify-start">
-    <span class="text-violet-500 font-bold text-4xl text-center w-1/2 my-3 border-4 border-violet-500 p-4 bg-gray-900 rounded-3xl z-10 ">Trofeos</span>
+    <span class="text-violet-500 font-bold text-4xl text-center w-1/2 my-3 border-4 border-violet-500 p-4 bg-gray-900 rounded-3xl z-10 ">Logros</span>
     <div class="h-full w-full lg:w-5/6 bg-gray-600 -translate-y-14 border-4 rounded-3xl border-violet-500 pt-20">
-      <div class="grid grid-cols-3 gap-6 h-5/6">
+      <div class="grid grid-cols-3 gap-6 h-5/6 overflow-y-scroll overflow-x-hidden">
         <span>ds</span>
         <span>ds</span>
         <span>ds</span>
@@ -38,11 +39,11 @@
         <span>ds</span>
       </div>
       <div class="flex flex-col items-center justify-center h-1/6 bg-gray-900 border-t-2 border-t-yellow-500  pt-2 rounded-b-3xl text-gray-200 w-full bg-opacity-80">
-        <span>Completed: </span>
+        <span>Progreso:</span>
         <div class="w-full h-full flex flex-row">
-          <span class="w-1/2 font-semibold text-2xl">12/48</span>
+          <span class="w-1/2 font-semibold text-2xl text-yellow-500">12/48</span>
                   <button class="w-1/3 h-1/2 rounded-full mt-3 text-black bg-zinc-400  hover:bg-yellow-500 hover:text-white transition-all duration-500 border-2 border-transparent hover:border-white ">Ver todos</button>
-          <span class="w-1/2 font-semibold text-2xl">37%</span>
+          <span class="w-1/2 font-semibold text-2xl text-yellow-500">37%</span>
         </div>
 
 
